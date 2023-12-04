dd9360c791a1e0b7e370156e0531568c99824454
Andrea <abrighi@bsdsoftware.it>
2023-12-04 17:35:23 +0100

    feat(gateway): add module for kafka

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PixelGridController } from './app.controller';
+import { PixelGridService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'GRIDS_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'app-gateway',
+            brokers: ['kafka:9092'],
+          },
+          consumer: {
+            groupId: 'kafka-microservices',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [PixelGridController],
+  providers: [PixelGridService],
 })
 export class AppModule {}
