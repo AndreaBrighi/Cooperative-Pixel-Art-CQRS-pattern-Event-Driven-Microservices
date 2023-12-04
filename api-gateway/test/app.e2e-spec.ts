398c3b7c6cf5c214095290623c73e46b1572ec33
Andrea <abrighi@bsdsoftware.it>
2023-12-04 17:37:53 +0100

    test(gateway): update  test

--- api-gateway/test/app.e2e-spec.ts
+++ api-gateway/test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PointDto } from './../src/dto/PointDTO';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -15,10 +16,24 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/api/ (GET)', () => {
+  it('/api/pixel-grids (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/pixel-grids')
       .expect(200)
-      .expect('Hello World!');
+      .expect(['Grid1']);
+  });
+
+  it('/api/pixel-grids/Grid1 (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/pixel-grids/Grid1')
+      .expect(200)
+      .expect('0');
+  });
+  it('/api/pixel-grids/Grid1/color-pixel/000000 (GET)', () => {
+    return request(app.getHttpServer())
+      .post('/pixel-grids/Grid1/color-pixel/000000')
+      .send(new PointDto(0, 0))
+      .expect(201)
+      .expect('0');
   });
 });
