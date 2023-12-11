e970ef93be92248bc5d2e491aceb658354af1b32
Andrea <abrighi@bsdsoftware.it>
2023-12-11 17:59:35 +0100

    chore(dashboard): remove rabbitmq duplicated client

--- dashboard-view/src/infrastructures/rabbitmq.module.ts
+++ dashboard-view/src/infrastructures/rabbitmq.module.ts
@@ -1,24 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PixelGridsService } from './rabbitmq.service';
 import { DashboardRepository } from '../core/dashboard-repository';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'DASHBOARD_EVENT_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://rabbitmq:5672'],
-          queue: 'cats_queue',
-          queueOptions: {
-            durable: false,
-          },
-        },
-      },
-    ]),
-  ],
+  imports: [],
   providers: [
     {
       provide: DashboardRepository,
