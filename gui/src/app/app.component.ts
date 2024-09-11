aa8d82768838b9fc8f5b6d09699adcbf785445c4
Andrea <abrighi@bsdsoftware.it>
2024-09-11 14:07:16 +0200

    feat(gui): add mqtt library in gui

--- gui/src/app/app.component.ts
+++ gui/src/app/app.component.ts
@@ -1,21 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PixelGridComponent } from './pixel-grid/pixel-grid.component';
 import { ColorPickerModule } from 'ngx-color-picker';
+import { Subscription } from 'rxjs';
+import { IMqttMessage, MqttService } from 'ngx-mqtt';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PixelGridComponent, ColorPickerModule],
+  imports: [RouterOutlet, PixelGridComponent, ColorPickerModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'gui';
   color: string = '#000000';
 
-  constructor(){
+  private subscription: Subscription |undefined = undefined;
+  topicname: string = "";
+  msg: string = "";
+  isConnected: boolean = false;
 
+  constructor(private _mqttService: MqttService) { }
+
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  subscribeNewTopic(): void {
+    console.log('inside subscribe new topic')
+    this.subscription = this._mqttService.observe(this.topicname).subscribe((message: IMqttMessage) => {
+      this.msg = message.payload.toString()+1;
+      console.log('msg: ', message)
+    });
+    console.log('subscribed to topic: ' + this.topicname)
+  }
+
+  sendmsg(): void {
+    // use unsafe publish for non-ssl websockets
+    this._mqttService.unsafePublish(this.topicname, this.msg, { qos: 1, retain: true })
+    this.msg = ''
   }
 }
--- gui/src/app/app.config.ts
+++ gui/src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { MqttModule, IMqttServiceOptions } from "ngx-mqtt";
+import { FormsModule } from '@angular/forms';
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'localhost',
+  port: 15675,
+  path: '/ws',
+  protocol: 'ws'
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    FormsModule,
+    importProvidersFrom(MqttModule.forRoot(MQTT_SERVICE_OPTIONS))
+  ]
 };
