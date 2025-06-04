6ee10ccbffff633541902016a3932142f2274156
Andrea <abrighi@bsdsoftware.it>
2025-06-04 15:27:35 +0200

    chore(pixelgrid): add mongodb dependncy

--- pixelgrid/src/app.module.ts
+++ pixelgrid/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { PixelGridController } from './controllers/app.controller';
 import { PixelGridUseCasesModule } from './usecases/pixel-grid-usecases.module';
 import { PixelGridServicesModule } from './services/pixel-grids.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PixelGridUseCasesModule, PixelGridServicesModule],
+  imports: [
+    PixelGridUseCasesModule,
+    PixelGridServicesModule,
+    MongooseModule.forRoot('mongodb://localhost/pixelgrid', {
+      user: 'root',
+      pass: 'example',
+    }),
+  ],
   controllers: [PixelGridController],
   providers: [],
 })
